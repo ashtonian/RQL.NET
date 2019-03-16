6687ccf2b19ab80649533e7b967203f79c4fc6e5
ashtonian <github@ashtonkinslow.com>
2019-03-16 15:00:06 -0500

    refactor flow, start attribute implementations

--- tests/Attributes.cs
+++ tests/Attributes.cs
@@ -3,14 +3,14 @@ using System;
 [AttributeUsage(AttributeTargets.Class)]
 public class Filterable : System.Attribute
 {
-    private string[] _props;
+    internal string[] _props;
     public Filterable(params string[] props) { _props = props; }
 }
 
 [AttributeUsage(AttributeTargets.Class)]
 public class Sortable : System.Attribute
 {
-    private string[] _props;
+    internal string[] _props;
     public Sortable(params string[] props) { _props = props; }
 }
 
@@ -26,17 +26,11 @@ public class Ignore : System.Attribute
 }
 public class Ops
 {
-    [AttributeUsage(AttributeTargets.Property)]
-    public class Allowed : System.Attribute
-    {
-        private string[] _ops;
-        public Allowed(params string[] ops) { _ops = ops; }
-    }
 
     [AttributeUsage(AttributeTargets.Property)]
     public class Disallowed : System.Attribute
     {
-        private string[] _ops;
+        internal string[] _ops;
         public Disallowed(params string[] ops) { _ops = ops; }
     }
 }
@@ -44,14 +38,14 @@ public class Ops
 [AttributeUsage(AttributeTargets.Property)]
 public class ColumnName : System.Attribute
 {
-    private string _name;
+    internal string _name;
     public ColumnName(string name) { _name = name; }
 }
 
 [AttributeUsage(AttributeTargets.Property)]
 public class FieldName : System.Attribute
 {
-    private string _name;
+    internal string _name;
     public FieldName(string name) { _name = name; }
 }
 
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -10,14 +10,32 @@ using Xunit;
 
 namespace tests
 {
+    public class TestClass2
+    {
+        public int X_Int { get; set; }
+        public string X_String { get; set; }
+        public bool X_Bool { get; set; }
+        public DateTime X_DateTime { get; set; }
+    }
+
+    public class TestClass
+    {
+        public long T_Long { get; set; }
+        public int T_Int { get; set; }
+        public string T_String { get; set; }
+        public bool T_Bool { get; set; }
+        public DateTime T_DateTime { get; set; }
+        // public TestClass2 T_SubClass { get; set; }
+    }
+
     public class UnitTest1
     {
         [Fact]
-        public void TestFieldSpec()
+        public void ClassSpecBuilder()
         {
-            var spec = new ClassSpec(typeof(TestClass));
-            var spec2 = new ClassSpec<TestClass>();
 
+            var specBuilder = new ClassSpecBuilder();
+            var classSpec = specBuilder.Build(typeof(TestClass));
             // var p = new Parser(spec);
 
             // var (str, parms, error) = p.ParseQuery(raw);
@@ -66,11 +84,11 @@ namespace tests
                     Name = "account",
                 },
             };
-            var spec = new ClassSpec(fields);
-            var p = new Parser(spec);
+            // var spec = new ClassSpec(fields);
+            // var p = new Parser(spec);
 
-            var (str, parms, error) = p.ParseQuery(raw);
-            Console.WriteLine(str, parms, error);
+            // var (str, parms, error) = p.ParseQuery(raw);
+            // Console.WriteLine(str, parms, error);
 
             // "WHERE city  =  TLV  AND (( city  =  TLV ) OR ( city  =  NYC )) AND  account  LIKE  =   % github %  "
         }
@@ -81,6 +99,7 @@ namespace tests
 public interface IOpMapper
 {
     (string, bool) GetDbOp(string rqlOp);
+    HashSet<String> GetSupportedOps(Type type);
 }
 public class SqlMapper : IOpMapper
 {
@@ -98,6 +117,12 @@ public class SqlMapper : IOpMapper
         {RqlOp.NIN,SqlOp.NIN},
         {RqlOp.IN,SqlOp.IN},
     };
+
+    public HashSet<string> GetSupportedOps(Type type)
+    {
+        throw new NotImplementedException();
+    }
+
     (string, bool) IOpMapper.GetDbOp(string rqlOp)
     {
         if (rqlOp == null || rqlOp.Trim().Length < 0) return (null, false);
@@ -108,42 +133,59 @@ public class SqlMapper : IOpMapper
     }
 }
 
-public class TestClass2
-{
-    public int X_Int { get; set; }
-    public string X_String { get; set; }
-    public bool X_Bool { get; set; }
-    public DateTime X_DateTime { get; set; }
-}
 
-public class TestClass
-{
-    public int T_Int { get; set; }
-    public string T_String { get; set; }
-    public bool T_Bool { get; set; }
-    public DateTime T_DateTime { get; set; }
-    public TestClass2 T_SubClass { get; set; }
-}
 
 public class ClassSpecBuilder
 {
+    private readonly Func<string, string> columnNamer;
+    private readonly Func<string, string> fieldNamer;
+    private readonly IOpMapper opMapper;
+
     public ClassSpec Build(Type t)
     {
         var _fields = new Dictionary<string, FieldSpec>() { };
 
         var properties = t.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+        var classAttributes = t.GetCustomAttributes(true);
+        var classFilter = classAttributes?.OfType<Filterable>()?.First();
+        var classSortable = classAttributes?.OfType<Filterable>()?.First();
 
-        // if (property.GetCustomAttributes(true)
-        // .Any(attr => attr.GetType().Name == typeof(IgnoreSelectAttribute).Name || attr.GetType().Name == typeof(NotMappedAttribute).Name)) ;
-        // continue;
+        // if is class use name resolver and concat fields to class TODO:
 
-        //  _fields = fields.ToDictionary(f => f.Name, f => f);
-        return new ClassSpec();
+        // class attribute validator 
+        // class attribute converter
+
+        foreach (var p in properties)
+        {
+            var attributes = p.GetCustomAttributes(true);
+
+            var ignore = attributes?.OfType<Ignore>()?.First();
+            var ignoreSort = attributes?.OfType<Ignore.Sort>()?.First();
+            var ignoreFilter = attributes?.OfType<Ignore.Filter>()?.First();
+
+            var opsBlacklist = attributes?.OfType<Ops.Disallowed>()?.First();
+            var columnName = attributes?.OfType<ColumnName>()?.First();
+            var fieldName = attributes?.OfType<FieldName>()?.First();
+
+            var _field = new FieldSpec
+            {
+                Ops = opMapper.GetSupportedOps(p.PropertyType),
+                PropType = p.PropertyType,
+                ColumnName = columnName?._name ?? columnNamer(p.Name),
+                Name = fieldName?._name ?? fieldNamer(p.Name),
+                Converter = null,               // ?? attribute that looks for IConverter on field, then class
+                Validator = null,               // ?? attribute that looks for IValidator on field, then class
+            };
+
+        }
+
+        return new ClassSpec()
+        {
+            Fields = _fields,
+            Converter = null, // ?? staticRef default converter
+            Validator = null, // ?? staticRef
+        };
     }
-}
-public class ClassSpecResolver
-{
-
 }
 
 public class ClassSpec
@@ -151,50 +193,30 @@ public class ClassSpec
     public Dictionary<string, FieldSpec> Fields { get; set; }
     public Func<string, Type, object, IError> Validator { get; set; }
     public Func<string, Type, object, (object, IError)> Converter { get; set; }
-    public Func<string, string> ColumnNamer { get; set; }
 }
 
 public class FieldSpec
 {
     public string Name { get; set; }
     public string ColumnName { get; set; }
-    public IEnumerable<string> Ops { get; set; }
+    public HashSet<string> Ops { get; set; }
     public Type PropType { get; set; }
     public Func<string, Type, object, IError> Validator { get; set; }
     public Func<string, Type, object, (object, IError)> Converter { get; set; }
+    // public ClassSpec _classSpec; // TODO: use for flat object a sub queries 
 }
 
 
 // TODO: cache reflection 
-// TODO: hard typed errors/exceptions
-// TODO: pull params to dict 
-// TODO: dapper + sql raw examples 
-// TODO: pull out op mapper func from rql to sql to allow mongo or other.
-// TODO: column mapping 
-// TODO: limit offset sort 
-// TODO: feature throw vs return err
-// TODO: not and nor 
 // TODO: gentle validation flag ie if invalid op or field is entered drop and move 
-// TODO: flat nested object
 // TODO: benchmark on top of json deserialization 
 // TODO: example integrations: dapper, dapper.crud/extensions(limit+ofset), sql mapper
 // TODO: package(core,.sql) + build 
 // TODO: try with DI framework, mvc/web-api + multi target
-// TODO: allow token prefix config 
-// TODO: tokenizer ? vs @param1
-// TODO: pull out where and return {limit,offset,sort,expression, args}
-// TODO: eventually try and pull out json deserializer
-// TODO: refactor error messages to provide parent token details
-// TODO: eventually add nested query support
+// TODO: pull out json deserializer and use own tree {left, v, right, isField, isOp}
 // TODO: js + typescript lib
-// TODO: convert enum classes to const
 // TODO: investigate json ops
-// TODO: dynamic spec so that it doesn't have to be global for the same struct, sometimes might not want that.
-// TODO: c# 7.0 is expressions https://www.danielcrabtree.com/blog/152/c-sharp-7-is-operator-patterns-you-wont-need-as-as-often
-// TODO: deal with tokenizer state and parse() should be static. 
-// TODO: feature - class level default validator, converter 
-// TODO: class spec should contain all relevant information required to parse a given class the generation of such class should be extracted, I think this is the cache point, can pass in for one off. 
-
+// TODO: validate not + and right side is object and, or/nor is array
 
 public class ParseResult
 {
@@ -203,136 +225,152 @@ public class ParseResult
     public string SortExpression { get; set; }
     public string FilterExpression { get; set; }
     public Dictionary<string, object> FilterParameters { get; set; }
-    public IEnumerable<IErrors> Errors { get; set; }
 }
 
-
-public class ResultState
+internal class ParseState
 {
-    public Dictionary<string, object> FilterParameters { get; set; }
-    public List<IErrors> Errors { get; set; }
-    public StringBuilder Query { get; set; }
-
+    public ParseState(IParameterTokenizer parameterTokenizer = null)
+    {
+        _parameterTokenizer = parameterTokenizer;
+    }
+    public readonly StringBuilder _query = new StringBuilder();
+    public readonly Dictionary<string, object> _filterParameters = new Dictionary<string, object>();
+    public readonly List<IError> _errors = new List<IError>();
+    public readonly IParameterTokenizer _parameterTokenizer;
 }
 
+public class Parser<T> : Parser
+{
+    // pass static class calls in constructor
+    public Parser() : base(null, null, null) { }
+}
 public class Parser
 {
-    public Func<IParameterTokenizer> TokenizerFactory { get; set; }
-    public ClassSpec ClassSpec { get; set; }
-    public Func<string, (string, bool)> OpResolver { get; set; }
+    private readonly Func<IParameterTokenizer> _tokenizerFactory;
+    private readonly ClassSpec _classSpec;
+    private readonly Func<string, string> _opResolver;
 
-    private static ParseResult ParseTerms(
-        Parser parser,
-        JContainer container,
-        string parentToken = null,
-        IParameterTokenizer tokenizer = null,
-        ResultState res = null
-    )
+    public Parser(ClassSpec classSpec, Func<string, string> opResolver, Func<IParameterTokenizer> tokenizerFactory)
     {
-        if (res == null) { res = new ResultState() { }; }
-        if (tokenizer == null) { parser.TokenizerFactory(); }
-
-        var fieldCount = 0;
-        foreach (var raw in container.Children())
-        {
-            var token = raw as JProperty;
-            // TODO:  if (fieldCount > 0 && fieldCount < termContainer.Count) query.Append($" {SqlOp.AND} ");
-            fieldCount++;
-
-            var field = parser.ClassSpec.Fields[token.Name.Trim()];
-            var nextTerm = token.Value as JContainer;
-
-            // Parse Field 
-            if (field != null)
-            {
-                // Right side of field is term, recursive call 
-                if (nextTerm != null)
-                {
-                    ParseTerms(parser, nextTerm, token.Name.Trim(), tokenizer, res);
-                }
-                // Right side is primitive and this is a return statement
-                else
-                {
-                    var columnName = field.ColumnName ?? parser.ClassSpec.ColumnNamer(field.Name);
-                    res.Query.Append($" {columnName} ");
-
-                    var jValue = token.Value as JValue;
-                    if (jValue == null)
-                    {
-                        res.Errors.Add(new Error("Unable to cast right side as primitive"));
-                        continue;
-                    }
-
-                    var (key, val, err) = getParameter(field, jValue.Value, tokenizer, parser.ClassSpec.Converter, parser.ClassSpec.Validator);
-                    if (err != null)
-                    {
-                        res.Errors.Add(err);
-                        continue;
-                    }
-                    res.FilterParameters.Add(key, val);
-
-                    res.Query.Append($" {parser.OpResolver(RqlOp.EQ)} ");
-                    res.Query.Append($" {key} ");
-                }
-
-                continue;
-            }
-
-            // Parse rql op
-            var isOp = RqlOp.IsOp(prop.Name);
-            if (isOp)
-            {
-                var op = prop.Name;
-                var (sqlOp, isSqlOp) = _opMapper.GetDbOp(op);
-                if (!isSqlOp) return (null, new Error("rql operation not supported in sql"));
-
-                // Right side of op is collection of terms recursive call
-                if (op == RqlOp.OR || op == RqlOp.AND)
-                {
-                    query.Append("(");
-                    if (nextTerm == null) return (null, new Error("expected collection of terms on right"));
-                    var jArr = prop?.Values()?.ToArray();
-                    for (var idx = 0; idx < jArr.Length; idx++)
-                    {
-                        if (idx > 0 && idx != jArr.Length) query.Append($" {sqlOp} ");
-                        if (jArr.Length > 0) query.Append("(");
-
-                        var childTerm = jArr[idx] as JContainer;
-                        ParseTerms(childTerm, query);
-
-                        if (jArr.Length > 0) query.Append(")");
-                    }
-                    query.Append(")");
-                }
-                // Right side is primitive collection and this is root return 
-                else if (op == RqlOp.IN || op == RqlOp.NIN)
-                {
-                    throw new NotImplementedException("IN + NIN not implemented");
-                }
-                // Right side is primitive and this is root return 
-                else
-                {
-                    if (parentField == null) return (null, new Error("missing expected field spec"));
-                    query.Append($" {parentField.Name} ");
-                    query.Append($" {sqlOp} ");
-                    var val = prop.Value as JValue;
-                    if (val == null) return (null, new Error("cannot cast to primitive, invalid value"));
-                    var paramToken = "";
-
-                    // query.Append($" {SqlOp.EQ} "); // TODO: don't always add 
-                    query.Append($" {paramToken} ");
-                }
-                continue;
-            }
-
-            return (null, new Error("not valid rql operation or model field name, invalid property"));
-        }
-
-
-        return (query, null);
+        _tokenizerFactory = tokenizerFactory;
+        _classSpec = classSpec;
+        _opResolver = opResolver;
     }
 
+    public (ParseResult, IEnumerable<IError>) Parse(string toParse)
+    {
+        try
+        {
+            var jsonObject = JsonConvert.DeserializeObject(toParse);
 
+            return ParseTerms(this, jsonObject as JContainer, RqlOp.AND); // TODO: or if array
+        }
+        catch (Exception e)
+        {
+            throw e; // TODO: 
+        }
+    }
+
+    private static (ParseResult, IEnumerable<IError>) ParseTerms(
+        Parser parser,
+        JContainer container,
+        string parentToken,
+        ParseState state = null
+    )
+    {
+        if (container == null) { } // invalid rigt side or json serialiize
+        if (state == null) { state = new ParseState(parser._tokenizerFactory()); };
+
+
+        for (var idx = 0; idx < container.Count; idx++)
+        {
+            var raw = container[idx];
+            var token = raw as JProperty;
+            var leftSide = token.Name;
+
+            if (idx > 0 && idx < container.Count - 1 && RqlOp.IsOp(parentToken))
+            {
+                if (parentToken == RqlOp.OR || parentToken == RqlOp.AND) state._query.Append($" {parser._opResolver(parentToken)} ");
+                if (parentToken == RqlOp.NOT) state._query.Append($" {parser._opResolver(RqlOp.AND)} ");
+                if (parentToken == RqlOp.NOR) state._query.Append($" {parser._opResolver(RqlOp.OR)} ");
+            }
+
+            var field = parser._classSpec.Fields[leftSide];
+            var nextTerm = token.Value as JContainer;
+
+            // Parse Field value is recursive 
+            if (field != null && nextTerm != null)
+            {
+                ParseTerms(parser, nextTerm, leftSide);
+            }
+            // Parse recursive op
+            else if (
+                RqlOp.IsOp(leftSide)
+                && leftSide == RqlOp.OR
+                || leftSide == RqlOp.AND
+                || leftSide == RqlOp.NOT
+                || leftSide == RqlOp.NOR
+            )
+            {
+                if (container.Count > 0) state._query.Append($" {leftSide} (");
+                ParseTerms(parser, nextTerm, leftSide);
+                if (container.Count > 0) state._query.Append($")");
+            }
+            // Right side is primitive and and field is left side or parent 
+            else if (RqlOp.IsOp(leftSide) || field != null)
+            {
+                var parentField = parser._classSpec.Fields[parentToken];
+                var jProp = token.Value as JProperty;
+                resolveNode(parser, field ?? parentField, leftSide, jProp, state);
+            }
+            else
+            {
+                state._errors.Add(new Error($"invalid field or op {leftSide}, parent:{parentToken}"));
+            }
+        }
+
+        return (
+            new ParseResult
+            {
+                FilterExpression = state._query.ToString(),
+                FilterParameters = state._filterParameters,
+            },
+            state._errors
+        );
+    }
+
+    private static void resolveNode(
+        Parser parser,
+        FieldSpec fieldSpec,
+        string rqlOp,
+        JProperty val,
+        ParseState state
+    )
+    {
+        var sqlOp = parser._opResolver(rqlOp);
+        if (sqlOp == null) { state._errors.Add(new Error($"{rqlOp} is not supported.")); return; }
+
+        if (!fieldSpec.Ops.Contains(rqlOp)) { state._errors.Add(new Error($"{fieldSpec.Name} does not support {rqlOp}.")); return; }
+
+        var preVal = prepPrim(val, rqlOp == RqlOp.IN || rqlOp == RqlOp.NIN);
+        var (key, processedVal, err) = getParameter(fieldSpec, preVal, state._parameterTokenizer, parser._classSpec.Converter, parser._classSpec.Validator);
+        if (err != null) { state._errors.Add(err); return; }
+
+        state._filterParameters.Add(key, processedVal);
+        state._query.Append($" {fieldSpec.ColumnName} {sqlOp} {key} ");
+    }
+
+    private static object prepPrim(JProperty jProp, bool isArray = false)
+    {
+        // take jcontainer 
+        // type to jvalue or jprop 
+        if (isArray) throw new NotImplementedException();
+
+        var jValue = jProp.Value as JValue;
+
+
+        return jValue;
+    }
 
 
     private static (string, object, IError) getParameter(
@@ -343,6 +381,7 @@ public class Parser
         Func<string, Type, object, IError> _defaultValidator = null
     )
     {
+        if (val == null) return (null, null, new Error("could not parse right side as valid primitive"));
         var converter = field.Converter ?? _defaultConverter;
 
         if (converter != null)
@@ -362,20 +401,5 @@ public class Parser
         var parameterName = _tokenizer.GetToken(field.Name, field.PropType);
 
         return (parameterName, val, null);
-    }
-
-    public (string, Dictionary<string, object>, IError) ParseQuery(string query)
-    {
-        try
-        {
-            var jsonObject = JsonConvert.DeserializeObject(query);
-
-            var (queryBuilder, err) = ParseTerms(jsonObject as JContainer);
-            return (queryBuilder.ToString(), null, err);
-        }
-        catch (Exception e)
-        {
-            return (null, null, new Error(e.ToString())); // CAUGHT exception likely from json
-        }
     }
 }
